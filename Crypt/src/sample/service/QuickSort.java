d40654c22b3285ca511bbfd4f8df80c5e195ff87
Ruslan <Prosony@bk.ru>
2018-02-20 23:26:34 +0300

    update2

--- Crypt/src/sample/service/QuickSort.java
+++ Crypt/src/sample/service/QuickSort.java
@@ -1,48 +1,61 @@
 package sample.service;
 
-
-
 public class QuickSort {
 	private ABC abc = new ABC();
-
+	private int number = 0;
 	public void sortDuoMassive(char[][] source, int height , int width){
 		char[] key = new char[width];
 
 			for (int indexWidth = 0; indexWidth < width; indexWidth++) {
 				key[indexWidth] = source[0][indexWidth];
-
 			}
-		char[] result = sorting(key, 0, width-1, (width-1)/2 );
+		sorting(key, 0, width-1); //char[] result =
 			System.out.println("done");
-			for(int index = 0; index < width; index++){
-				System.out.print(result[index]);
-			}
+//			for(int index = 0; index < width; index++){
+//				System.out.print(result[index]);
+//			}
 	}
 
-	private char[] sorting(char[] source, int begin , int end, int support ){
-		int left = begin;
-		int right = end;
-		int supportNumberChar = abc.getNumberABC(source[support]); //support element
+	private void sorting(char[] source, int left, int right){
 
+		int index = partition(source, left, right);
+
+		if (left < index - 1){
+			System.out.println("#"+number+" left: "+left+" index "+index);
+			number++;
+			sorting(source, left, index - 1);
+		}
+		if (index < right) {
+			System.out.println("#"+number+" index "+index+" right: "+right);
+			number++;
+			sorting(source, index, right);
+		}
+//		return source;
+	}
+
+	private int partition(char[] source, int leftPart, int rightPart) {
+		int left = leftPart;
+		int right = rightPart;
+		int support = (left + right)/2;
+		int supportNumberChar = abc.getNumberABC(source[support]); //support element
 		while(left < right){
 			while(abc.getNumberABC(source[left]) < supportNumberChar){
-					left++;
+				left++;
 			}
 			while(abc.getNumberABC(source[right]) > supportNumberChar){
-					right--;
+				right--;
 			}
+			System.out.println("supportNumberChar: "+ supportNumberChar);
+			System.out.println("[left]"+left+" < "+right+"[right]");
 			if (left < right){
-				int changeLeft = left + 1;
-				int changeRight = right - 1;
-				char afk = source[changeLeft];
-				source[changeLeft] = source[changeRight];
-				source[changeRight] = afk;
+				char afk = source[left];
+				source[left] = source[right];
+				source[right] = afk;
+				left++;
+				right--;
 			}
 		}
-
-		if (begin < right){sorting(source, begin, right, (begin+right)/2);}
-		if (end > left){sorting(source, left, end, (end + left)/2);}
-
-		return source;
+		System.out.println("Part 3");
+		return left;
 	}
 }
