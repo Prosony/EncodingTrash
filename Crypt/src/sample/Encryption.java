06365050f1aae1f973e6d047733717da59f6a7c8
Prosony <Prosony@bk.ru>
2018-03-06 01:39:54 +0300

    update.3

--- Crypt/src/sample/Encryption.java
+++ Crypt/src/sample/Encryption.java
@@ -1,29 +1,27 @@
 package sample;
 
+import sample.service.EncryptionService;
 import sample.service.QuickSort;
 import sample.service.SortBySymbol;
 
 public class Encryption {
 
 	public String firstEncryption(int key, String source){
-		int count;
-		if (source.length() % key > 0){
-			count = source.length() / key + 1;
-		}else{
-			count = source.length() / key;
-		}
+
+		EncryptionService service = new EncryptionService();
+		int height = service.getHeightSecond(key, source);
 
 		System.out.println("source.length()" + source.length());
 		System.out.println("key:" + key);
-		System.out.println("count:" + count);
+		System.out.println("height:" + height);
 
-		char[][] array = new char[key][count];
+		char[][] array = new char[key][height];
 		char[] sourceArray = source.toCharArray();
 		int index_of_char = 0;
 
 		System.out.println("#____PARSING_START____#");
 		for (int i_key=0; i_key < key; i_key++){
-			for (int i_count=0; i_count < count; i_count++){
+			for (int i_count=0; i_count < height; i_count++){
 
 				if (index_of_char < source.length()){
 					array[i_key][i_count] = sourceArray[index_of_char];
@@ -37,20 +35,16 @@ public class Encryption {
 			}
 		}
 		System.out.println("\n#____PARSING_END____#");
-		return convertArray(array, key, count, 1);
+		return service.convertArray(array, key, height, 1);
 	}
 
 	public String secondEncryption(String key, String source){
 
 		char[] keyArray = key.toCharArray();
 		int width = keyArray.length;
+		EncryptionService service = new EncryptionService();
+		int height = service.getHeightSecond(width,source);
 
-		int height;
-		if((source.length()+width)%width > 0){
-			height = (source.length()+width)/width + 1;
-		}else{
-			height = (source.length()+width)/width;
-		}
 		char[][] array = new char[height][width];
 		char[] sourceArray = source.toCharArray();
 		int sourceWidth = sourceArray.length;
@@ -77,31 +71,8 @@ public class Encryption {
 		bySymbol.sort(array, result, height, width);
 
 		System.out.println("#INFO [Encryption] [secondEncryption] After");
-		return convertArray(array, height, width, 2);
+		return service.convertArray(array, height, width, 2);
 	}
 
-	private String convertArray(char[][] result , int height, int width, int method){
-		StringBuilder builder = new StringBuilder();
-		System.out.println("#INFO [Encryption] [secondEncryption] #____RESULT:____#");
 
-		switch (method){
-			case 1:
-				for(int indexLine = 0; indexLine < width; indexLine ++){
-					for (int indexColumn = 0; indexColumn < height; indexColumn++){
-						builder.append(result[indexColumn][indexLine]);
-						System.out.print(result[indexColumn][indexLine]);
-					}
-				}
-			break;
-			case 2:
-				for (int indexHeight = 1; indexHeight < height; indexHeight++){
-					for (int indexWidth = 0; indexWidth < width; indexWidth++) {
-						builder.append(result[indexHeight][indexWidth]);
-						System.out.print(result[indexHeight][indexWidth]);
-					}
-				}
-				break;
-		}
-		return  builder.toString();
-	}
 }
---
+++ Crypt/src/sample/service/EncryptionService.java
@@ -0,0 +1,50 @@
+package sample.service;
+
+public class EncryptionService {
+
+    public int getHeightFirst(int key, String source){
+        int height;
+        if (source.length() % key > 0){
+            height = source.length() / key + 1;
+        }else{
+            height = source.length() / key;
+        }
+        return height;
+    }
+
+    public int getHeightSecond(int width,  String source){
+        int height;
+        int length = source.length();
+        if((source.length()+width)%width > 0){
+            height = (length+width)/width + 1;
+        }else{
+            height = (source.length()+width)/width;
+        }
+        return  height;
+    }
+
+    public String convertArray(char[][] result , int height, int width, int method){
+        StringBuilder builder = new StringBuilder();
+        System.out.println("#INFO [Encryption] [secondEncryption] #____RESULT:____#");
+
+        switch (method){
+            case 1:
+                for(int indexLine = 0; indexLine < width; indexLine ++){
+                    for (int indexColumn = 0; indexColumn < height; indexColumn++){
+                        builder.append(result[indexColumn][indexLine]);
+                        System.out.print(result[indexColumn][indexLine]);
+                    }
+                }
+                break;
+            case 2:
+                for (int indexHeight = 1; indexHeight < height; indexHeight++){
+                    for (int indexWidth = 0; indexWidth < width; indexWidth++) {
+                        builder.append(result[indexHeight][indexWidth]);
+                        System.out.print(result[indexHeight][indexWidth]);
+                    }
+                }
+                break;
+        }
+        return  builder.toString();
+    }
+}
